ca89f572ee2b13873502635e6a409eb22a88254f
U1F419 <ika.u1f991@gmail.com>
2020-04-26 19:51:11 +0900

    Reactに移植

---
+++ src/Hex/Hex.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Hex.scss";
+
+function Hexagon() {
+  return (
+    <div className="hexagon">
+      <div className="top"></div>
+      <div className="middle"></div>
+      <div className="bottom"></div>
+    </div>
+  );
+}
+
+function Row(count: number, isEven: boolean) {
+  const hexagons = [];
+  for (let i = 0; i < count; i++) {
+    hexagons.push(Hexagon());
+  }
+
+  let className = "row";
+  if (isEven) {
+    className += " even"
+  }
+  return <div className={className}>{hexagons}</div>;
+}
+
+export function Grid(row: number, column: number) {
+  const rows = [];
+  for (let i = 0; i < row; i++) {
+    rows.push(Row(column, i % 2 !== 0));
+  }
+  return <div className="grid">{rows}</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as hex from "./Hex/Hex";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>{hex.Grid(10, 10)}</React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
